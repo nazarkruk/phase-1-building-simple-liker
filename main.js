9cc0e82ad735f1a1789ecee7bb15903b816f57ee
Nazar Kruk <nazarkruk@icloud.com>
2022-02-17 01:18:40 -0500

    Done

--- main.js
+++ main.js
@@ -4,6 +4,47 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const modal = document.querySelector('#modal')
+modal.className = 'hidden'
+
+const allhearts = document.querySelectorAll('.like-glyph')
+const hearts = [...allhearts]
+
+
+for (const heart of hearts) {
+  
+  heart.addEventListener("click", (el)=>{
+    mimicServerCall()
+      .then(heartChange(heart))
+      .catch(err);
+  });
+
+}
+
+
+function heartChange(heart){
+  //const heart = document.querySelector('.like-glyph')
+    if (heart.innerText === EMPTY_HEART){
+      heart.innerText = FULL_HEART
+      return heart.className = 'activated-heart'
+    } else if(heart.innerText === FULL_HEART ) {
+      heart.innerText = EMPTY_HEART
+      return heart.classList.remove('activated-heart')
+    }
+  }
+  
+ 
+
+function err (){
+  modal.classList.remove('hidden')
+  setTimeout(()=>{
+    modal.className = 'hidden'
+  }, 3000)
+}
+
+
+
+
 
 
 
